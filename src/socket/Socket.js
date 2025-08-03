3c74e985a6ed11fad738a78aebe9633606f6f677
yashodhanAgale <yashodhanagale3@gmail.com>
2025-08-03 22:05:01 +0530

    vercel deployment

--- src/socket/Socket.js
+++ src/socket/Socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://telemedicine-backend-o4h3.onrender.com/", {
   withCredentials: true,
   transports: ["websocket"], // optional for cleaner WS only
 });
